9128c98054d329a668dda8d08140d7b2e3ca1b8d
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2024-11-13 17:45:54 +0100

    Add two separate builds

--- build-ext.mjs
+++ build-ext.mjs
@@ -2,7 +2,7 @@ import { build } from 'esbuild'
 
 await build({
   entryPoints: ['./src/index.js'],
-  outfile: './dist/index.js',
+  outfile: './dist/index-ext.js',
   bundle: true,
   platform: "node",
   target: "node16",
